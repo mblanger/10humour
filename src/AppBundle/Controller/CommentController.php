d328e81bf0a4790fa30c1f3b222b78c3ef4bd358
Tom <tom.bacci@hotmail.fr>
2017-06-20 14:32:10 +0200

    Feat comment type

--- src/AppBundle/Controller/CommentController.php
+++ src/AppBundle/Controller/CommentController.php
@@ -2,7 +2,9 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Comment;
 use AppBundle\Entity\Post;
+use AppBundle\Form\CommentType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -10,8 +12,17 @@ class CommentController extends Controller
 {
     public function commentAction(Request $request, Post $post)
     {
+        $comment = new Comment();
+        $comment->setUser($this->getUser());
+        $comment->setPost($post);
+        $form = $this->createForm(CommentType::class, $comment);
+        $form->handleRequest($request);
+
+        dump($form->getData());
+
         return $this->render('AppBundle:Comment:comment.html.twig', array(
-            'post' => $post
+            'post' => $post,
+            'form' => $form->createView()
         ));
     }
 
